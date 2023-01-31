02af3d69b6bbc6741fd778d2466a27fa58d8f41f
Brandon Parsons <brandon@parsonstx.com>
2023-01-31 22:19:31 +0000

    Updated to use pip requirements file and include yaml
    Additionally added a variable and usage notes for the venv usage

--- machine-config/setup.sh
+++ machine-config/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+scriptdir=$(dirname $(realpath $0))
+venvname="Visionvenv"
+
 # Machine setup (Ubuntu)
 # Python3 venv: needed to create virtual environments
 # cmake, ninja, g++: needed for building opencv-python and pupil_apriltags packages
@@ -7,9 +10,15 @@
 sudo apt install python3.10-venv git ninja-build cmake g++ v4l-conf v4l-utils
 
 # Virtual environment creation
-python3 -m venv ~/Visionvenv --upgrade-deps
-source ~/Visionvenv/bin/activate
+python3 -m venv ~/${venvname} --upgrade-deps
+source ~/${venvname}/bin/activate
 
 # Virtual environment package installation
 # requires: cmake, ninja, g++
-pip3 install numpy pupil_apriltags opencv-python
+pip3 install -r ${scriptdir}/requirements.txt
+
+echo "Required Python tools have been installed in a virtual environment (venv) at: "
+echo " -> ~/${venvname} (aka $HOME/${venvname})"
+echo ""
+echo "To activate venv before executing any scripts, perform the following in your shell"
+echo " source ~/${venvname}/bin/activate"
